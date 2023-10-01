351484264f6331f7423b30fcacc34a798c51c621
Anton Kondratev <kondratev@elma-bpm.com>
2023-10-01 23:54:18 +0400

    init

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <h1>Index</h1>
+    <nuxt-img src="/1.jpg"/>
+
+<!--    malloc-->
+<!--    <nuxt-img src="/2.png"/> -->
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
